c183453ca11a4f85baebcf7ff4777897d64a3f5c
Swarup Yeole <76248886+swarupyeole11@users.noreply.github.com>
2023-01-31 17:15:28 +0530

    Update Arrays.kt

--- Arrays.kt
+++ Arrays.kt
@@ -1,1 +1,6 @@
+# Importnant Learning till Now 
+
+// # You can use $ symbol to display the varible value print("this is swarup $a and I love doing addition of ${a+b}") 
+// for an expression ou can use the curly braces
+
 print("hello")
